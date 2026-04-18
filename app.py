d71e2a40e70943d8c5114fcf5687527b34db654a
psautade-pixel <psautade@kkwagh.edu.in>
2026-04-18 11:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
-@app.route(&quot;/&quot;)
+
+@app.route("/")
 def home():
-return &quot;Hello World from Cloud ��&quot;
-if __name__ == &quot;__main__&quot;:
-app.run()
+    return "Hello World from Cloud"
+
+if __name__ == "__main__":
+    app.run(debug=True)
